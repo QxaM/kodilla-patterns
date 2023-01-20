14ed205a05b1a12190a634cb89fe66923d20a7a4
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-01-20 20:57:10 +0100

    15.2 deep copy example

--- patterns/src/main/java/com/kodilla/patterns/prototype/Board.java
+++ patterns/src/main/java/com/kodilla/patterns/prototype/Board.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public final class Board extends Prototype<Board> {
 
     private String name;
-    private final Set<TasksList> lists = new HashSet<>();
+    private Set<TasksList> lists = new HashSet<>();
 
     public Board(String name) {
         this.name = name;
@@ -36,6 +36,20 @@ public final class Board extends Prototype<Board> {
 
     public Board shallowCopy() throws CloneNotSupportedException {
         return super.clone();
+    }
+
+    public Board deepCopy() throws CloneNotSupportedException {
+        Board clonedBoard = super.clone();
+        clonedBoard.lists = new HashSet<>();
+
+        for (TasksList theList : lists) {
+            TasksList clonedList = new TasksList(theList.getName());
+            for (Task task : theList.getTasks()) {
+                clonedList.getTasks().add(task);
+            }
+            clonedBoard.getLists().add(clonedList);
+        }
+        return clonedBoard;
     }
 
     @Override
--- patterns/src/test/java/com/kodilla/patterns/prototype/BoardTestSuite.java
+++ patterns/src/test/java/com/kodilla/patterns/prototype/BoardTestSuite.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class BoardTestSuite {
 
@@ -43,8 +44,15 @@ public class BoardTestSuite {
         } catch (CloneNotSupportedException e) {
             System.out.println(e);
         }
-        System.out.println(board);
-        System.out.println(clonedBoard);
+
+        //making a depp copy of object board
+        Board deepClonedBoard = null;
+        try {
+            deepClonedBoard = board.deepCopy();
+            deepClonedBoard.setName("Project number 3");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
 
         //When
         board.getLists().remove(listToDo);
@@ -52,8 +60,11 @@ public class BoardTestSuite {
         //Then
         System.out.println(board);
         System.out.println(clonedBoard);
-
+        System.out.println(deepClonedBoard);
         assertEquals(2, board.getLists().size());
-        assertEquals(3, clonedBoard.getLists().size());
+        assertEquals(2, clonedBoard.getLists().size());
+        assertEquals(3, deepClonedBoard.getLists().size());
+        assertEquals(clonedBoard.getLists(), board.getLists());
+        assertNotEquals(deepClonedBoard.getLists(), board.getLists());
     }
 }
