40652403ba3c16c489c254d30bae8641cd3b1020
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-01-21 14:28:41 +0100

    15.6 added SimpleInvoice class and conditional breakpoint

--- kodilla-streams/src/main/java/com/kodilla/Main.java
+++ kodilla-streams/src/main/java/com/kodilla/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.kodilla;
 
 public class Main {
     public static void main(String[] args) {
---
+++ kodilla-testing/src/main/java/com/kodilla/stream/invoice/simple/Main.java
@@ -0,0 +1,7 @@
+package com.kodilla.stream.invoice.simple;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
